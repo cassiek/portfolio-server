3fbf087d116fd770d9492ec47793475c473dc3f5
cassiek <ckumpula123@gmail.com>
2024-12-11 01:06:41 -0500

    rename .json file to data.json. Finish adding info to data.json. Update routes, index.js to fix sending data functioning

--- routes/projects.js
+++ routes/projects.js
@@ -4,9 +4,14 @@ import fs from "fs";
 //import { v4 as uuidv4 } from "uuid";
 
 router.get("/", (_req, res) => {
-    console.log("get all projects api called");
-    const projectData = fs.readFileSync("./data/projects.json");
-    return res.status(200).json(projectData);
+
+    //console.log("get all projects api called");
+    const projectsData = fs.readFileSync("./data/data.json", "utf8");
+    //console.log("read file");
+    const parsedJsonData = JSON.parse(projectsData); // parsing converts to JS object? withot this I am sent rich text?
+    //console.log("parsed Json")
+    return res.status(200).json(parsedJsonData);
+    
 });
 
-export default router;
+export default router; 
